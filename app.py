9fe60d27a729596127c47f5bea1edcef613c7040
rodres101 <rodrigosergio93@gmail.com>
2023-02-19 09:02:14 -0400

    quit host

--- app.py
+++ app.py
@@ -8,5 +8,5 @@ def hello():
     return 'Hello, mundo'
 
 
-if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=8000, debug=True)
+# if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=8000, debug=True)
