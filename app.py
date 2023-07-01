56af729a58ac646314c7755e35a42dc3bfaddd91
rodres101 <rodrigosergio93@gmail.com>
2023-07-01 17:14:39 -0400

    sha values

--- app.py
+++ app.py
@@ -1,12 +1,47 @@
+"""
+sumary_line
+"""
 
 from flask import Flask
+from flask import render_template
+from flask import request
+from sha import encript_string
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, mundo'
+    """sumary_line"""
+    return render_template(
+        'index.html', 
+        
+    )
 
+@app.route('/sha', methods=['GET', 'POST'])
+def calculate_sha():
+    """sumary_line"""
+    form_value = request.form.get("formValue","")
+    ans = encript_string(hash_string=form_value)
 
-# if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=8000, debug=True)
+    if form_value != "":
+        return render_template(
+            'index.html', 
+            sha_value=ans,
+            form_value=form_value
+        )
+    else:
+        return render_template(
+            'index.html'
+        )
+    
+# class Item:
+#     def __init__(self) -> None:
+#         self.href = "href"
+#         self.caption  = "This is the caption"
+        
+if __name__ == '__main__':
+   app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True
+    )
--- sha.py
+++ sha.py
@@ -1,7 +1,5 @@
 import hashlib
 
-
-
 def encript_string(hash_string:str) -> str:
     """
     https://medium.com/@dwernychukjosh/sha256-encryption-with-python-bf216db497f9
@@ -11,9 +9,6 @@ def encript_string(hash_string:str) -> str:
     ).hexdigest()
     return sha_signature
 
-
-
-hash_string = 'confidential data'
-sha_signature = encript_string(hash_string)
-
-print(sha_signature)
+# hash_string = 'confidential data'
+# sha_signature = encript_string(hash_string)
+# print(sha_signature)
